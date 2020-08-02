aa24ee0e9ceab5c34dd418b775d4dacb59665872
Georg Kunz <georg.kunz@est.tech>
2020-08-02 22:27:32 +0200

    Adding pdf and idf for Ericsson pod 1
    This adds the pod descriptor file and installer descriptor file
    for Ericsson pod 1.
    
    Signed-off-by: Georg Kunz <georg.kunz@est.tech>
    Change-Id: Ie518f71a35b657ef4a7b17464cb8f0e4116a3771

---
+++ hw_config/ericsson-pod1/setup_network.sh
@@ -0,0 +1,8 @@
+# Adapt this script according to your network setup
+# TODO Get networking info from PDF & IDF
+# TODO Add support in infra engine to update nameserver, etc
+# files with correct info
+#!/bin/bash
+
+echo "no additional network setup needed"
+
