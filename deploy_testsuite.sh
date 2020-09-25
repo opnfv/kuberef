a48f479073cd0497bf587849cc3629b3ce90f626
Rihab Banday <rihab.banday@ericsson.com>
2020-09-25 14:28:27 +0200

    Adding scripts for running RC2 testcases
    This patch adds support for running RC2 Functest
    testcases suite on the K8s cluster deployed by
    Kuberef deployer.
    
    Change-Id: I7652aa817ad9b434235abf0fab9ba63f4ad2f23e
    Signed-off-by: Rihab Banday <rihab.banday@ericsson.com>

---
+++ deploy_testsuite.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) Ericsson AB and others
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Script to run RC2 testcases suite on the K8s cluster
+# deployed by deploy.sh. More info to be added in README
+
+set -o xtrace
+set -o errexit
+set -o nounset
+
+# Get path information
+CURRENTPATH=$(git rev-parse --show-toplevel)
+export CURRENTPATH
+
+# Source env variables & functions
+source "$CURRENTPATH/deploy.env"
+source "$CURRENTPATH/functions.sh"
+
+# ---------------------------------------------------------------------
+# Run RC2 Conformance Suite
+# ---------------------------------------------------------------------
+run_testsuite
