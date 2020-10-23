be3d03f0f79db76420f316965d8813a46d9c1e0b
Michael S. Pedersen <michaelx.pedersen@intel.com>
2020-10-23 12:24:43 +0000

    Add support for deploying to Equinix Metal bare-metal servers
    Adds an additional hw_config which is simplified (equinix-metal).
    Adds support for skipping HW provisioning (pre-provisioned through EM).
    Usage: deploy.sh [k8s]
    Adds a few new helper functions, and updates existing to support changes.
    
    Signed-off-by: Michael S. Pedersen <michaelx.pedersen@intel.com>
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    Change-Id: I2f84c662d183ba224e0edd8a218abe8b46c1fe45
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- functions.sh
+++ functions.sh
@@ -8,32 +8,55 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+info() {
+    _print_msg "INFO" "$1"
+}
+
+error() {
+    _print_msg "ERROR" "$1"
+    exit 1
+}
+
+_print_msg() {
+    echo "$(date +%H:%M:%S) - $1: $2"
+}
+
+assert_non_empty() {
+    if [ -z "$1" ]; then
+        error "$2"
+    fi
+}
+
 check_prerequisites() {
-    echo "Info  : Check prerequisites"
+    info "Check prerequisites"
+
+    #-------------------------------------------------------------------------------
+    # Check for DEPLOYMENT type
+    #-------------------------------------------------------------------------------
+    DEPLOYMENT=${DEPLOYMENT:-full}
+    if ! [[ "$DEPLOYMENT" =~ ^(full|k8s)$ ]]; then
+        error "Unsupported value for DEPLOYMENT ($DEPLOYMENT)"
+    fi
 
     #-------------------------------------------------------------------------------
     # We shouldn't be running as root
     #-------------------------------------------------------------------------------
-    if [[ "$(whoami)" == "root" ]]; then
-        echo "ERROR : This script must not be run as root!"
-        echo "        Please switch to a regular user before running the script."
-        exit 1
+    if [[ "$(whoami)" == "root" ]] && [[ "$DEPLOYMENT" != "k8s" ]]; then
+        error "This script must not be run as root! Please switch to a regular user before running the script."
     fi
 
     #-------------------------------------------------------------------------------
     # Check for passwordless sudo
     #-------------------------------------------------------------------------------
     if ! sudo -n "true"; then
-        echo "ERROR : passwordless sudo is needed for '$(id -nu)' user."
-        exit 1
+        error "passwordless sudo is needed for '$(id -nu)' user."
     fi
 
     #-------------------------------------------------------------------------------
     # Check if SSH key exists
     #-------------------------------------------------------------------------------
     if [[ ! -f "$HOME/.ssh/id_rsa" ]]; then
-        echo "ERROR : You must have SSH keypair in order to run this script!"
-        exit 1
+        error "You must have SSH keypair in order to run this script!"
     fi
 
     #-------------------------------------------------------------------------------
@@ -42,28 +65,13 @@ check_prerequisites() {
     sudo sed -i "s/^Defaults.*env_reset/#&/" /etc/sudoers
 
     #-------------------------------------------------------------------------------
-    # Check if Ansible is installed
+    # Check if some tools are installed
     #-------------------------------------------------------------------------------
-    if ! command -v ansible &> /dev/null; then
-        echo "ERROR : Ansible not found. Please install."
-        exit 1
-    fi
-
-    #-------------------------------------------------------------------------------
-    # Check is yq is installed
-    #-------------------------------------------------------------------------------
-    if ! command -v yq &> /dev/null; then
-        echo "ERROR : yq not found. Please install."
-        exit 1
-    fi
-
-    #-------------------------------------------------------------------------------
-    # Check is libvirt is installed
-    #-------------------------------------------------------------------------------
-    if ! command -v virsh &> /dev/null; then
-        echo "ERROR : Libvirt not found. Please install."
-        exit 1
-    fi
+    for tool in ansible yq virsh; do
+        if ! command -v "$tool" &> /dev/null; then
+            error "$tool not found. Please install."
+        fi
+    done
 
     #-------------------------------------------------------------------------------
     # Check if user belongs to libvirt's group
@@ -75,54 +83,91 @@ check_prerequisites() {
         libvirt_group+="d"
     fi
     if ! groups | grep "$libvirt_group"; then
-        echo "ERROR : $(id -nu) user doesn't belong to $libvirt_group group."
-        exit 1
+        error "$(id -nu) user doesn't belong to $libvirt_group group."
     fi
 }
 
-# Get jumphost VM IP
+# Get jumphost VM PXE IP
 get_host_pxe_ip() {
     local PXE_NETWORK
     local PXE_IF_INDEX
     local PXE_IF_IP
 
     host=$1
-    if [[ "$host" == "" ]]; then
-        echo "ERROR : get_ip - host parameter not provided"
-        exit 1
-    fi
+    assert_non_empty "$host" "get_ip - host parameter not provided"
 
-    PXE_NETWORK=$(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml  engine.pxe_network)
-    if [[ "$PXE_NETWORK" == "" ]]; then
-        echo "ERROR : PXE network for jump VM not defined in IDF."
-        exit 1
-    fi
+    PXE_NETWORK=$(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml engine.pxe_network)
+    assert_non_empty "$PXE_NETWORK" "PXE network for jump VM not defined in IDF."
 
     PXE_IF_INDEX=$(yq r "$CURRENTPATH"/hw_config/"${VENDOR}"/idf.yaml idf.net_config."$PXE_NETWORK".interface)
-    if [[ "$PXE_IF_INDEX" == "" ]]; then
-        echo "ERROR : Index of PXE interface not found in IDF."
-        exit 1
-    fi
+    assert_non_empty "$PXE_IF_INDEX" "Index of PXE interface not found in IDF."
 
     PXE_IF_IP=$(yq r "$CURRENTPATH"/hw_config/"${VENDOR}"/pdf.yaml "$host".interfaces["$PXE_IF_INDEX"].address)
-    if [[ "$PXE_IF_IP" == "" ]]; then
-        echo "ERROR : IP of PXE interface not found in PDF."
-        exit 1
-    fi
+    assert_non_empty "$PXE_IF_IP" "IP of PXE interface not found in PDF."
+
     echo "$PXE_IF_IP"
 }
 
+# Get public MAC for VM
+get_host_pub_mac() {
+    local PUB_NETWORK
+    local PUB_IF_INDEX
+    local PUB_IF_MAC
+
+    host=$1
+    if [[ "$host" == "" ]]; then
+        echo "ERROR : get_mac - host parameter not provided"
+        exit 1
+    fi
+
+    PUB_NETWORK=$(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml  engine.public_network)
+    if [[ "$PUB_NETWORK" == "" ]]; then
+        echo "ERROR : Public network for jump VM not defined in IDF."
+        exit 1
+    fi
+
+    PUB_IF_INDEX=$(yq r "$CURRENTPATH"/hw_config/"${VENDOR}"/idf.yaml idf.net_config."$PUB_NETWORK".interface)
+    if [[ "$PUB_IF_INDEX" == "" ]]; then
+        echo "ERROR : Index of public interface not found in IDF."
+        exit 1
+    fi
+
+    PUB_IF_MAC=$(yq r "$CURRENTPATH"/hw_config/"${VENDOR}"/pdf.yaml "$host".interfaces["$PUB_IF_INDEX"].mac_address)
+    if [[ "$PUB_IF_MAC" == "" ]]; then
+        echo "ERROR : MAC of public interface not found in PDF."
+        exit 1
+    fi
+    echo "$PUB_IF_MAC"
+}
+
+# Get jumphost VM IP
 get_vm_ip() {
-    ip=$(get_host_pxe_ip "jumphost")
+    if [[ "$DEPLOYMENT" == "full" ]]; then
+        ip=$(get_host_pxe_ip "jumphost")
+    else
+        mac=$(get_host_pub_mac "jumphost")
+        JUMPHOST_NAME=$(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/pdf.yaml jumphost.name)
+        ipblock=$(virsh domifaddr "$JUMPHOST_NAME" --full | grep "$mac" | awk '{print $4}' | tail -n 1)
+        if [[ "$ipblock" == "" ]]; then
+            echo "ERROR : IP subnet for VM not available"
+            exit 1
+        fi
+        ip="${ipblock%/*}"
+    fi
     echo "$ip"
 }
 
-
 # Copy files needed by Infra engine & BMRA in the jumphost VM
 copy_files_jump() {
+    vm_ip="$(get_vm_ip)"
     scp -r -o StrictHostKeyChecking=no \
     "$CURRENTPATH"/{hw_config/"$VENDOR"/,sw_config/"$INSTALLER"/} \
-    "$USERNAME@$(get_vm_ip):$PROJECT_ROOT"
+    "$USERNAME@${vm_ip}:$PROJECT_ROOT"
+    if [[ "$DEPLOYMENT" != "full" ]]; then
+        scp -r -o StrictHostKeyChecking=no \
+        ~/.ssh/id_rsa \
+        "$USERNAME@${vm_ip}:.ssh/id_rsa"
+    fi
 }
 
 # Host Provisioning
@@ -145,23 +190,34 @@ EOF
 
 # Setup networking on provisioned hosts (Adapt setup_network.sh according to your network setup)
 setup_network() {
-# Set Upper limit of number nodes in RI2 cluster (starting from 0)
-NODE_MAX_ID=$(($(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml --length idf.kubespray.hostnames)-1))
+    # Set Upper limit of number nodes in RI2 cluster (starting from 0)
+    NODE_MAX_ID=$(($(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml --length idf.kubespray.hostnames)-1))
 
-for idx in $(seq 0 "$NODE_MAX_ID")
-do
-    NODE_IP=$(get_host_pxe_ip "nodes[${idx}]")
-# SSH to jumphost
-    # shellcheck disable=SC2087
-    ssh -o StrictHostKeyChecking=no -tT "$USERNAME"@"$(get_vm_ip)" << EOF
+    for idx in $(seq 0 "$NODE_MAX_ID"); do
+        NODE_IP=$(get_host_pxe_ip "nodes[${idx}]")
+        # SSH to jumphost
+        # shellcheck disable=SC2087
+        ssh -o StrictHostKeyChecking=no -tT "$USERNAME"@"$(get_vm_ip)" << EOF
 ssh -o StrictHostKeyChecking=no root@${NODE_IP} \
     'bash -s' <  ${PROJECT_ROOT}/${VENDOR}/setup_network.sh
 EOF
-done
+    done
+}
+
+# Get IPs of target nodes (used for installing dependencies)
+get_target_ips() {
+    yq r "$CURRENTPATH"/hw_config/"$VENDOR"/pdf.yaml nodes[*].interfaces[*].address
 }
 
 # k8s Provisioning (currently BMRA)
 provision_k8s() {
+    ansible_cmd='/bin/bash -c "'
+    if [[ "$DEPLOYMENT" == "k8s" ]]; then
+        ansible-playbook -i "$CURRENTPATH"/sw_config/bmra/inventory.ini "$CURRENTPATH"/playbooks/pre-install.yaml
+        ansible_cmd+='pip install --upgrade pip==9.0.3; yum -y remove python-netaddr; pip install netaddr==0.7.19; '
+    fi
+    ansible_cmd+='ansible-playbook -i /bmra/inventory.ini /bmra/playbooks/cluster.yml"'
+
     # shellcheck disable=SC2087
     ssh -o StrictHostKeyChecking=no -tT "$USERNAME"@"$(get_vm_ip)" << EOF
 # Install BMRA
@@ -177,8 +233,10 @@ if [ ! -d "${PROJECT_ROOT}/container-experience-kits" ]; then
     git clone --recurse-submodules --depth 1 https://github.com/intel/container-experience-kits.git -b v1.4.1 ${PROJECT_ROOT}/container-experience-kits/
     cp -r ${PROJECT_ROOT}/container-experience-kits/examples/group_vars ${PROJECT_ROOT}/container-experience-kits/
 #TODO Remove this once the reported issue is fixed in the next BMRA Release
-    sed -i '/\openshift/a \    extra_args: --ignore-installed PyYAML' \
+    if [[ "$DEPLOYMENT" == "full" ]]; then
+        sed -i '/\openshift/a \    extra_args: --ignore-installed PyYAML' \
         ${PROJECT_ROOT}/container-experience-kits/roles/net-attach-defs-create/tasks/main.yml
+    fi
 fi
 cp ${PROJECT_ROOT}/${INSTALLER}/inventory.ini \
     ${PROJECT_ROOT}/container-experience-kits/
@@ -188,6 +246,6 @@ sudo docker run --rm \
 -e ANSIBLE_CONFIG=/bmra/ansible.cfg \
 -v ${PROJECT_ROOT}/container-experience-kits:/bmra \
 -v ~/.ssh/:/root/.ssh/ rihabbanday/bmra-install:centos \
-ansible-playbook -i /bmra/inventory.ini /bmra/playbooks/cluster.yml
+${ansible_cmd}
 EOF
 }
