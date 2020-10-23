4b07cb95b18cb3fe6d4d41a0a38d3b9d6ea50f30
Michael S. Pedersen <michaelx.pedersen@intel.com>
2020-10-23 12:24:43 +0000

    Add support for deploying to Equinix Metal bare-metal servers
    Adds an additional hw_config which is simplified (equinix-metal).
    Adds support for skipping HW provisioning (pre-provisioned through EM).
    Usage: deploy.sh [k8s]
    Adds a few new helper functions, and updates existing to support changes.
    
    Signed-off-by: Michael S. Pedersen <michaelx.pedersen@intel.com>
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    Change-Id: I2f84c662d183ba224e0edd8a218abe8b46c1fe45
    Signed-off-by: Victor Morales <v.morales@samsung.com>

--- deploy.sh
+++ deploy.sh
@@ -33,20 +33,17 @@ source "$CURRENTPATH/deploy.env"
 # ---------------------------------------------------------------------
 # bootstrap install prerequisites
 # ---------------------------------------------------------------------
-ansible-playbook -i "$CURRENTPATH"/inventory/localhost.ini \
-    "$CURRENTPATH"/playbooks/bootstrap.yaml
+run_playbook bootstrap
 
 # ---------------------------------------------------------------------
 # Create jump VM from which the installation is performed
 # ---------------------------------------------------------------------
-ansible-playbook -i "$CURRENTPATH"/inventory/localhost.ini \
-    "$CURRENTPATH"/playbooks/jump-vm.yaml
+run_playbook jump-vm
 
 # ---------------------------------------------------------------------
 # Create BMRA config based on IDF and PDF
 # ---------------------------------------------------------------------
-ansible-playbook -i "$CURRENTPATH"/inventory/localhost.ini \
-    "$CURRENTPATH"/playbooks/bmra-config.yaml
+run_playbook bmra-config
 
 # ---------------------------------------------------------------------
 # Copy files needed by Infra engine & BMRA in the jumphost VM
@@ -55,13 +52,12 @@ copy_files_jump
 
 # ---------------------------------------------------------------------
 # Provision remote hosts
-# ---------------------------------------------------------------------
-provision_hosts
-
-# ---------------------------------------------------------------------
 # Setup networking (Adapt according to your network setup)
 # ---------------------------------------------------------------------
-setup_network
+if [[ "$DEPLOYMENT" == "full" ]]; then
+    provision_hosts
+    setup_network
+fi
 
 # ---------------------------------------------------------------------
 # Provision k8s cluster (currently BMRA)
