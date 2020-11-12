7306dcfdaab4ecb17f3ea97985c890cd353241e1
Victor Morales <v.morales@samsung.com>
2020-11-11 20:52:09 -0500

    Fix linting issues
    This change fixes some linting issues accumulated from previous
    patchsets and ignored by the CI jobs.
    
    Signed-off-by: Victor Morales <v.morales@samsung.com>
    Change-Id: I0bf2e67aee786505e9c01570d4d8d99df62b1f9f

--- functions.sh
+++ functions.sh
@@ -145,19 +145,18 @@ EOF
 
 # Setup networking on provisioned hosts (Adapt setup_network.sh according to your network setup)
 setup_network() {
-# Set Upper limit of number nodes in RI2 cluster (starting from 0)
-NODE_MAX_ID=$(($(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml --length idf.kubespray.hostnames)-1))
+    # Set Upper limit of number nodes in RI2 cluster (starting from 0)
+    NODE_MAX_ID=$(($(yq r "$CURRENTPATH"/hw_config/"$VENDOR"/idf.yaml --length idf.kubespray.hostnames)-1))
 
-for idx in $(seq 0 "$NODE_MAX_ID")
-do
-    NODE_IP=$(get_host_pxe_ip "nodes[${idx}]")
-# SSH to jumphost
-    # shellcheck disable=SC2087
-    ssh -o StrictHostKeyChecking=no -tT "$USERNAME"@"$(get_vm_ip)" << EOF
+    for idx in $(seq 0 "$NODE_MAX_ID"); do
+        NODE_IP=$(get_host_pxe_ip "nodes[${idx}]")
+        # SSH to jumphost
+        # shellcheck disable=SC2087
+        ssh -o StrictHostKeyChecking=no -tT "$USERNAME"@"$(get_vm_ip)" << EOF
 ssh -o StrictHostKeyChecking=no root@${NODE_IP} \
     'bash -s' <  ${PROJECT_ROOT}/${VENDOR}/setup_network.sh
 EOF
-done
+    done
 }
 
 # k8s Provisioning (currently BMRA)
