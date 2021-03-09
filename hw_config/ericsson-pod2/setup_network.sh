d1306fe9ae78e4be6575baaf2d56940b7ecbb533
Rihab Banday <rihab.banday@ericsson.com>
2021-03-09 15:26:54 +0100

    Updated PDF and IDF for Ericsson Hardware
    This change updates the config files for Ericsson POD 1
    and removes existing Ericsson POD 2 configuration based
    on recent hardware and software updates.
    
    Change-Id: I4d26be681b4d805029e5de9c91f96a7634974990
    Signed-off-by: Rihab Banday <rihab.banday@ericsson.com>

--- hw_config/ericsson-pod2/setup_network.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Adapt this script according to your network setup
-# TODO Get networking info from PDF & IDF
-# TODO Add support in infra engine to update nameserver, etc
-# files with correct info
-
-echo "no additional network setup needed"
-
