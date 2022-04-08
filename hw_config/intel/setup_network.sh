4e88b589d6cfbdbd394ea67539e827ef7c07813c
Michael Pedersen <michaelx.pedersen@intel.com>
2022-04-08 12:20:08 +0200

    [KUB-62] Make DNS configuration persistent
    Changes DNS configuration from /etc/resolv.conf to network-scripts.
    This should ensure DNS stays persistent on system
    
    Signed-off-by: Michael Pedersen <michaelx.pedersen@intel.com>
    Change-Id: Ie1d987b93b462c1a81a4a6852364462b80c2e00e

--- hw_config/intel/setup_network.sh
+++ hw_config/intel/setup_network.sh
@@ -9,8 +9,9 @@
 # TODO Add support in infra engine to update nameserver, etc
 # files with correct info
 
-echo nameserver 8.8.8.8 > /etc/resolv.conf
 sed -i 's/NM_CONTROLLED=yes/NM_CONTROLLED=no/g' \
 /etc/sysconfig/network-scripts/ifcfg-eth2
 echo GATEWAY=10.10.190.1 >> /etc/sysconfig/network-scripts/ifcfg-eth2
+echo DNS1=8.8.8.8 >> /etc/sysconfig/network-scripts/ifcfg-eth2
+echo DNS2=8.8.4.4 >> /etc/sysconfig/network-scripts/ifcfg-eth2
 ifup eth2
