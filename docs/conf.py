486015325b01283f31595abd8fbdbf93530f6a5a
xudan16 <xudan16@huawei.com>
2023-02-13 15:36:55 +0800

    Applying Piccolo theme
    Signed-off-by: xudan16 <xudan16@huawei.com>
    Change-Id: If169f3250aaa8ba326919c98fb36170eb8402232
    Reviewed-on: https://gerrit.opnfv.org/gerrit/c/kuberef/+/73866
    Tested-by: jenkins-ci <jenkins-opnfv-ci@opnfv.org>
    Reviewed-by: Gergely Csatari <gergely.csatari@nokia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,47 @@
-# SPDX-FileCopyrightText: 2021 Anuket contributors
-#
-# SPDX-License-Identifier: Apache-2.0
+project = 'kuberef'
+html_title = "kuberef"
+copyright = '2023, Anuket. Licensed under CC BY 4.0'
+author = 'Anuket Project of Linux Foundation Networking'
 
-""" for docs
-"""
+exclude_patterns = [
+    '.tox',
+    'README.rst'
+]
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel'
+]
+html_theme = "piccolo_theme"
+linkcheck_ignore = [
+    'http://127.0.0.1'
+]
+intersphinx_mapping = {
+    'cntt': ('https://cntt.readthedocs.io/en/latest/', None),
+    'ref_model': ('https://cntt.readthedocs.io/projects/rm/en/latest/', None),
+    'ref_arch_kubernetes': ('https://cntt.readthedocs.io/projects/ra2/en/latest/', None),
+    'ref_cert_RC2': ('https://cntt.readthedocs.io/projects/rc2/en/latest/', None),
+    'ref_impl_RI2': ('https://cntt.readthedocs.io/projects/ri2/en/latest/', None)
+}
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 3
+numfig = True
+numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
+                 'code-block': 'Listing %s', 'section': 'Section %s'}
 
-# pylint: disable=import-error
-# flake8: noqa
-from docs_conf.conf import *
-copyright = '2021, Anuket. Licensed under CC BY 4.0'
+html_static_path = ['_static']
+templates_path = ['_templates']
+html_css_files = [
+    'custom.css',
+]
+
+html_show_sourcelink = False
+html_theme_options = {
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
+
+# Inverse png
+html_logo = '_static/anuket-logo.png'
+html_favicon = '_static/favicon.ico'
